ccee67bb9e6361d26e5ba2d077063c475ee7b0d3
froliceric0914 <wzhao.eric@gmail.com>
2020-09-28 22:40:58 -0400

    use slice function to solve Chunk problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,21 +8,34 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// function chunk(array, size) {
+//     const chunkArray = [];
+//     for (let e of array) {
+//         console.log('e', e);
+//         const last = chunkArray[chunkArray.length - 1];
+//         console.log('last', last);
+//         if (!last || last.length === size) {
+//             chunkArray.push([e]);
+//             console.log('chunkArray1', chunkArray);
+//         } else {
+//             last.push(e);
+//             console.log('chunkArray2', chunkArray);
+//         }
+//     }
+//     return chunkArray;
+// }
+
+//Array.slice(n,m) return to an array, starting from index n to the element< m
+//e.g. array.slice(0,2), return to array[0,1]
+
 function chunk(array, size) {
-    const chunkArray = [];
-    for (let e of array) {
-        console.log('e', e);
-        const last = chunkArray[chunkArray.length - 1];
-        console.log('last', last);
-        if (!last || last.length === size) {
-            chunkArray.push([e]);
-            console.log('chunkArray1', chunkArray);
-        } else {
-            last.push(e);
-            console.log('chunkArray2', chunkArray);
-        }
+    const chunked = [];
+    let index = 0;
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
     }
-    return chunkArray;
+    return chunked;
 }
 
 console.log(chunk([1, 2, 3, 4, 5], 2));
